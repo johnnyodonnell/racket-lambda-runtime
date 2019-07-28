e83c458b592a4206c5e528c790b77f7513ebcf3e
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2019-07-28 10:31:49 -0700

    Add publishing script and current version

---
+++ README.md
@@ -0,0 +1,3 @@
+Current runtime version:
+rn:aws:lambda:us-west-2:005501675243:layer:racket-runtime:1
+
--- make.sh
+++ make.sh
@@ -7,3 +7,8 @@ docker build . --tag runtime-image
 docker create -it --name runtime-container runtime-image
 docker cp runtime-container:/runtime bootstrap
 
+zip -r lambda.zip bootstrap
+aws lambda publish-layer-version --layer-name racket-runtime \
+    --description "Racket custom runtime" \
+    --zip-file fileb://lambda.zip
+
